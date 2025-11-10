5908b78ae23fcf807c431e0eceb67a327f5bc473
Znyd <mailboxofnabil@gmail.com>
2025-11-11 03:34:28 +0600

    folder and file creation

---
+++ setup.py
@@ -0,0 +1,14 @@
+from config import PROJECT_ROOT
+import pandas as pd
+
+store_dir = PROJECT_ROOT / "store"
+prompt_dir = PROJECT_ROOT / "prompts"
+store_dir.mkdir(exist_ok=True)
+prompt_dir.mkdir(exist_ok=True)
+
+primitive_response_path = store_dir / "primitive_response.csv"
+if not primitive_response_path.exists():
+    primitive_response_path.touch(exist_ok=True)
+    pd.DataFrame(
+        columns=pd.Index(["video_topic", "scenario", "size", "response"])
+    ).to_csv(primitive_response_path, index=False)
